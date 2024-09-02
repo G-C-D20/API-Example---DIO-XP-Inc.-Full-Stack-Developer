f00a5d5792c512a6b6eddd7bbda0416d78868363
Giovanne Cabriotti Danna <giovannecabriottidanna@gmail.com>
2024-09-02 12:04:46 -0300

    API EXAMPLE V1

---
+++ Migrations/20240901005731_NewTabelaContatos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiAlpha.Migrations
+{
+    public partial class NewTabelaContatos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
